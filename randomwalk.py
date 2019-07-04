f2726e89dd74f0c160c475acb5cce6584bb67302
Konstantinos Angelou <angelou.konstantinos@gmail.com>
2019-07-04 23:34:27 +0300

    Converted to class

--- randomwalk.py
+++ randomwalk.py
@@ -7,24 +7,40 @@ Created on Thu Jul  4 23:09:19 2019
 
 import random
 
+class RandomWalker():
+    '''This is a random walker simulator'''
+    def __init__(self, x=0, y=0):
+        self.x = 0
+        self.y = 0
+        #random.seed(myseed)
+    
+    def move(self):
+        tmp = random.random()
+        if tmp < 0.25:
+            self.x -= 1
+        elif tmp < 0.5:
+            self.x += 1
+        elif tmp < 0.75:
+            self.y -= 1
+        else:
+            self.y += 1
+    
+    def calculateR2(self):
+        r2 = self.x*self.x + self.y*self.y
+        return r2
+
+
 def main():
-    random.seed(1234)
+    #random.seed(1234)
     steps, runs = 1000, 10000
     mysum = 0
     for run in range(runs):
-        x, y = 0, 0
+        rwalker = RandomWalker(0,0)
         for step in range(steps):
-            tmp = random.random()
-            if tmp<0.25:
-                x -= 1
-            elif tmp<0.5:
-                x += 1
-            elif tmp<0.75:
-                y -= 1
-            else:
-                y += 1
-        r2 = x*x + y*y
+            rwalker.move()
+        r2 = rwalker.calculateR2()
         mysum += r2
+            
     print('R^2 = {}'.format(mysum/runs))
 
 if __name__ == "__main__":
