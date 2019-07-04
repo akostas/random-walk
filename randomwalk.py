8ef53ad561ec73d9b38729255d1a03094ad06596
Konstantinos Angelou <angelou.konstantinos@gmail.com>
2019-07-04 23:21:52 +0300

    Converted to function

--- randomwalk.py
+++ randomwalk.py
@@ -7,20 +7,26 @@ Created on Thu Jul  4 23:09:19 2019
 
 import random
 
-steps, runs = 1000, 10000
-mysum = 0
-for run in range(runs):
-    x, y = 0, 0
-    for step in range(steps):
-        tmp = random.random()
-        if tmp<0.25:
-            x -= 1
-        elif tmp<0.5:
-            x += 1
-        elif tmp<0.75:
-            y -= 1
-        else:
-            y += 1
-    r2 = x*x + y*y
-    mysum += r2
-print('R^2 = {}'.format(mysum/runs))
+def main():
+    random.seed(1234)
+    steps, runs = 1000, 10000
+    mysum = 0
+    for run in range(runs):
+        x, y = 0, 0
+        for step in range(steps):
+            tmp = random.random()
+            if tmp<0.25:
+                x -= 1
+            elif tmp<0.5:
+                x += 1
+            elif tmp<0.75:
+                y -= 1
+            else:
+                y += 1
+        r2 = x*x + y*y
+        mysum += r2
+    print('R^2 = {}'.format(mysum/runs))
+
+if __name__ == "__main__":
+    # execute only if run as a script
+    main()
